c820658bc7d11aded45dcc7a75ce03722142a623
sagivalia11@gmail.com <sagivalia11@gmail.com>
2022-07-09 09:03:26 +0300

    Emailing works

--- Controllers/register.js
+++ Controllers/register.js
@@ -6,7 +6,9 @@ import bcrypt from 'bcryptjs'
 import cookie from 'cookie-parser'
 import db from './../Models/dbConn.js'
 import acc from './../Models/accounts.js'
-
+import jwt from 'jsonwebtoken'
+import emailer from 'nodemailer'
+import smtpserver, { SMTPServer } from 'smtp-server' 
 const registerGet = (req,res) => {
     console.log("good get")
     return res.status(200).json({
@@ -15,16 +17,15 @@ const registerGet = (req,res) => {
 }
 const registerPost = async (req,res) => {
     //TODO: Integrate session functionality
-
-
-
     const registree = req.body;
 
-    console.log(`test: data from front-end\n:`);
-    console.log(registree);
-
+    //console.log(`test: data from front-end\n:`);
+    //console.log(registree);
     const acc_found = await acc.findAll({where: {email_addr: registree.email_addr}})
     
+
+
+
     if(acc_found.length > 0){
         
         return res.status(400).json({
@@ -32,6 +33,40 @@ const registerPost = async (req,res) => {
         })
     }
     else{
+        const smtp = new SMTPServer()
+        
+        //Works now
+        const transport = emailer.createTransport({
+            service: 'gmail',
+            port:465,
+            secure: true,
+            secureConnection: false,
+            auth: {
+                user: 'lsrpacc9@gmail.com',
+                pass: 'qxsjzzgnwdrcxlgt', 
+            },
+            tls:{
+                rejectUnAuthorized:true
+            }
+            
+        })
+            
+        
+        var message = {
+                    from: "sender@gmail.com",
+                    to: "sagivalia11@gmail.com",
+                    subject: "Message title",
+                    html: `<p>Your verification code is 12345</p>`
+                    };
+
+         transport.sendMail(message)
+         .then(response => {
+            console.log(`Email: email sent:\n ${response}`);
+         })
+         .catch(err => {
+            console.log(`Error: \n ${err}`);
+         })
+
         const encrypedPass = await bcrypt.hash(registree.password, 10);
         acc.create({
             first_name: registree.first_name,
@@ -59,6 +94,13 @@ const registerPost = async (req,res) => {
         })
     }
 
+}
+
+
+function between(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
 }
 
 
