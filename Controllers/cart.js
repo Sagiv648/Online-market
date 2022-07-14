0d99b1222f3a82c0b991882b0a4131e2796cec91
sagivalia11@gmail.com <sagivalia11@gmail.com>
2022-07-14 18:23:15 +0300

    more work on the cart router

--- Controllers/cart.js
+++ Controllers/cart.js
@@ -8,14 +8,11 @@ import order from './../Models/order.js'
 dotenv.config();
 //TODO:
 /*
-1. add a post request to add to the cart(orders table)
-2. add functionality in the main route to check if any products by their id are in stock ->
-    if they are not in stock, alert user
-3. add a delete request to delete from the cart (orders table)
 */
-
 const cartRouter = express.Router();
 
+//2. add functionality in the main route to check if any products by their id are in stock ->
+//   if they are not in stock, alert user
 cartRouter.get('/', (req,res) => {
 
     req.sessionStore.get(req.session.id, async (err, session) => {
@@ -27,6 +24,8 @@ cartRouter.get('/', (req,res) => {
             }
 
             const ordersById = await order.findAll({where: {account_id: session.userid.id}})
+            
+    
             return res.status(200).json({
                 username: session.userid.username,
                 cart: ordersById
@@ -42,6 +41,92 @@ cartRouter.get('/', (req,res) => {
 })
 
 
+
+//1. add a post request to add to the cart(orders table)
+cartRouter.post('/append', async (req,res) => {
+
+    req.sessionStore.get(req.session.id, async (err, session) => {
+        if(!session){
+            return res.status(400).json({
+                fault: "access denied"
+            })
+        }
+        const {pid, pname, price, amount} = req.body;
+
+        if(!amount){
+            return res.status(400).json({
+                fault : "no amount specified"
+            })
+        }
+        order.create({
+            account_id: session.userid.id,
+            product_id: pid,
+            amount: amount
+        })
+        .then(result => {
+            return res.status(200).json({
+                msg: "item added to cart",
+                result
+            })
+        })
+        .catch(err => {
+            return res.status(400).json({
+                fault: "could be an invalid id"
+            })
+        })
+        
+
+    })
+    
+})
+
+
+cartRouter.delete('/delete', (req,res) => {
+
+    req.sessionStore.get(req.session.id, async (err, session) => {
+        if(!session){
+            return res.status(400).json({
+                fault: "access denied"
+            });
+        }
+
+        const {pid, amount} = req.body;
+        const currentRow = await order.findAll({where: {account_id: session.userid.id, product_id: pid}});
+        if(currentRow.length == 0){
+            return res.status(400).json({
+                fault: "product not in the cart"
+            })
+        }
+        const details = currentRow[0];
+        if(amount >= currentRow[0].get('amount')){
+            
+            const destryoed = await order.destroy({where: {account_id: session.userid.id, product_id: pid}})
+            return res.status(200).json({
+                msg: `removed all of the amount of the product with id ${details.product_id}`
+            })
+        }
+        else{
+            const amountToUpdate = details.amount - amount;
+            order.update({amount: amountToUpdate}, {where: {account_id: session.userid, product_id: pid}})
+            .then(result => {
+                return res.status(200).json({
+                    msg: `removed ${details.amount} from product with id ${details.product_id}`
+                })
+            })
+            .catch(err => {
+                return res.status(500).json({
+                    fault: "server fault"
+                })
+            })
+            
+        }
+
+    })
+
+    
+
+
+})
 
 
 export default cartRouter;
