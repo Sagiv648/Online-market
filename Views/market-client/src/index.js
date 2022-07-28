4c9fed42404c0639741a03bebfb6e60ae6f7c098
sagivalia11@gmail.com <sagivalia11@gmail.com>
2022-07-28 08:03:34 +0300

    edited

--- Views/market-client/src/index.js
+++ Views/market-client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore, ConfigureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+
+      <App />
+    
+    
+
   </React.StrictMode>
 );
 
--- index.js
+++ index.js
@@ -62,7 +62,12 @@ app.use(session({
 }))
 
 app.use(cookieParser())
-
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 // app.set('views', 'ejs');
 // app.set('views', 'views');
 
