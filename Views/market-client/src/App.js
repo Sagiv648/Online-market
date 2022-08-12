47e6084d5d8accfbf0c503d71401ebe4e4661278
sagivalia11@gmail.com <sagivalia11@gmail.com>
2022-08-12 10:18:35 +0300

    work on front

--- Views/market-client/src/App.js
+++ Views/market-client/src/App.js
@@ -2,25 +2,55 @@
 import './App.css';
 import axios from 'axios';
 import {useEffect} from 'react'
+import {useState} from 'react'
+import 'react-router-dom'
+import { BrowserRouter, Route, Router } from 'react-router-dom';
+
+
+const useFetch = (url) => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const fetched = await axios.get(url);
+      setData(fetched.data);
+    }
+    fetch();
+  },[url])
+
+  return [data];
+}
+
 
 
 
 function App() {
 
-  useEffect(()=> {
+  const [data] = useFetch(`${process.env.REACT_APP_BASE_URL}/login`);
+  // const [obj, setObj] = useState({one: "", two: ""});
 
-    const fetch = async () => {
-      const d = await axios.get("http://localhost:2000/register");
-      console.log(d);
-    }
-    fetch();
-  })
+  // useEffect(()=> {
 
+  //   const fetch = async () => {
+  //     const d = await axios.get(`${process.env.REACT_APP_BASE_URL}/`);
+  //     //console.log(d.data);
+  //     setObj({one: d.data.welcome, two: d.data.msg});
+  //     //console.log(obj.msg);
+  //   }
+  //   fetch();
+  // },[])
 
+  const test = ()=> {
+    console.log(data);
+  }
+  test();
   return (
-    <div className="App">
+    <div>
+      <h1>{data}</h1>
       hello
     </div>
+    
   );
 }
 
